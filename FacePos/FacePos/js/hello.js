5c58a9769a86a322aa97731da5e7bbe6ed1203f4
Matt Dotson <matt.dotson@gmail.com>
2015-06-11 18:27:32 -0700

    updated the face recognition

--- FacePos/FacePos/js/hello.js
+++ FacePos/FacePos/js/hello.js
@@ -9,6 +9,7 @@
     var recordState = false;
     var storageFile;
     var photosPerSecond = 10;
+    var timer;
 
 
     var page = WinJS.UI.Pages.define("/hello.html", {
@@ -32,14 +33,36 @@
     // Begin initialization.
     function initialization() {
         displayMessage("Initialization started.");
-        enumerateCameras();
+        var setupCameraPromise = enumerateCamerasAsync()
+            .then(initMediaCaptureAsync)
+            .done(watchForPatrons);
     }
 
+    function watchForPatrons() {
+        //timer = setInterval(function () { 
+        if (!isAnalyzing) {
+            isAnalyzing = true;
+            capturePhotoAsync()
+                .then(detectAsync, errorHandler)
+                .then(identifyAsync, errorHandler)
+                .done(function (result) {
+                    isAnalyzing = false;
+                    if (result) {
+                        if (result.isNew) {
+                            trainModel();
+                        }
+                        //WinJS.Navigation.navigate("/ManageTab.html", result);
+                        document.location = "/ManageTab.html";
+                    }
+                }, errorHandler);
+        }
+        //}, 1000);
+    }
 
     // Identify available cameras.
-    function enumerateCameras() {
+    function enumerateCamerasAsync() {
         var deviceInfo = Windows.Devices.Enumeration.DeviceInformation;
-        deviceInfo.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture).then(function (devices) {
+        return deviceInfo.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture).then(function (devices) {
             // Add the devices to deviceList
             if (devices.length > 0) {
 
@@ -47,9 +70,17 @@
                     deviceList.push(devices[i]);
                 }
 
-                initCaptureSettings();
-                initMediaCapture();
-                
+                captureInitSettings = null;
+                captureInitSettings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
+                captureInitSettings.audioDeviceId = "";
+                captureInitSettings.videoDeviceId = "";
+                captureInitSettings.streamingCaptureMode = Windows.Media.Capture.StreamingCaptureMode.audioAndVideo;
+                captureInitSettings.photoCaptureSource = Windows.Media.Capture.PhotoCaptureSource.auto;
+                if (deviceList.length > 0)
+                    captureInitSettings.videoDeviceId = deviceList[0].id;
+
+                //initMediaCapture();
+
                 displayMessage("Initialization complete.");
 
             } else {
@@ -60,57 +91,41 @@
 
 
     // Initialize the MediaCaptureInitialzationSettings.
-    function initCaptureSettings() {
-        captureInitSettings = null;
-        captureInitSettings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
-        captureInitSettings.audioDeviceId = "";
-        captureInitSettings.videoDeviceId = "";
-        captureInitSettings.streamingCaptureMode = Windows.Media.Capture.StreamingCaptureMode.audioAndVideo;
-        captureInitSettings.photoCaptureSource = Windows.Media.Capture.PhotoCaptureSource.auto;
-        if (deviceList.length > 0)
-            captureInitSettings.videoDeviceId = deviceList[0].id;
-    }
+    //function initCaptureSettings() {
+    //}
 
-
-    // Create a profile.
-    function createProfile() {
-        profile = Windows.Media.MediaProperties.MediaEncodingProfile.createMp4(
-            Windows.Media.MediaProperties.VideoEncodingQuality.hd720p);
-    }
 
     // Create and initialze the MediaCapture object.
-    function initMediaCapture() {
+    function initMediaCaptureAsync() {
         oMediaCapture = null;
         oMediaCapture = new Windows.Media.Capture.MediaCapture();
-        oMediaCapture.initializeAsync(captureInitSettings).then(function (result) {
-            createProfile();
+        return oMediaCapture.initializeAsync(captureInitSettings).then(function (result) {
+            //        profile = Windows.Media.MediaProperties.MediaEncodingProfile.createMp4(
+            //Windows.Media.MediaProperties.VideoEncodingQuality.hd720p);
             var video = document.getElementById("previewVideo");
             video.src = URL.createObjectURL(oMediaCapture, { oneTimeOnly: true });
             video.play();
             displayMessage("Preview started");
-            var timer = setInterval(function () { capturePhoto() }, 1000);
         }, errorHandler);
     }
 
-    function capturePhoto() {
-        if (!isAnalyzing) {
-            isAnalyzing = true;
-            Windows.Storage.KnownFolders.picturesLibrary.createFileAsync("cameraCapture.png", Windows.Storage.CreationCollisionOption.replaceExisting).then(function (photoFile) {
-                var photoFormat = Windows.Media.MediaProperties.ImageEncodingProperties.createPng();
-                photoFormat.width = 200;
-                photoFormat.height = 200;
-                oMediaCapture.capturePhotoToStorageFileAsync(photoFormat, photoFile).then(function (result) {
-                    displayMessage("Captured");
-                    detect(photoFile);
-                }, errorHandler);
-            });
-        }
+    function capturePhotoAsync() {
+
+        return Windows.Storage.KnownFolders.picturesLibrary.createFileAsync("cameraCapture.png", Windows.Storage.CreationCollisionOption.replaceExisting).then(function (photoFile) {
+            var photoFormat = Windows.Media.MediaProperties.ImageEncodingProperties.createPng();
+            photoFormat.width = 200;
+            photoFormat.height = 200;
+            return oMediaCapture.capturePhotoToStorageFileAsync(photoFormat, photoFile).then(function (result) {
+                displayMessage("Captured");
+                return photoFile;
+            }, errorHandler);
+        });
     }
 
-    function detect(photoFile) {
-        photoFile.openAsync(Windows.Storage.FileAccessMode.read).then(function (stream) {
+    function detectAsync(photoFile) {
+        return photoFile.openAsync(Windows.Storage.FileAccessMode.read).then(function (stream) {
             var blob = MSApp.createBlobFromRandomAccessStream("image/png", stream);
-            WinJS.xhr({
+            return WinJS.xhr({
                 type: "POST",
                 url: "https://api.projectoxford.ai/face/v0/detections/?analyzesAge=true",
                 data: blob,
@@ -118,32 +133,31 @@
                     "Content-Type": "application/octet-stream",
                     "Ocp-Apim-Subscription-Key": "d3151dd7f3314a848baa820db31fac11"
                 }
-            }).done(
+            }).then(
                 function completed(xhr) {
                     var detections = JSON.parse(xhr.responseText);
-                    if (detections) {
+                    if (detections && detections.length) {
                         var faceId = detections[0].faceId;
                         if (faceId) {
-                            identify([faceId]);
+                            return [faceId];
                         }
                     }
                 },
                 function error(request) {
                     displayMessage(request);
-
                     isAnalyzing = false;
                 });
         }, errorHandler);
     }
 
-    function identify(faceIds) {
+    function identifyAsync(faceIds) {
         var idReq = {
             faceIds: faceIds,
             personGroupId: 1,
-            maxNumOfCandidatesReturned: 3 
+            maxNumOfCandidatesReturned: 3
         };
 
-        WinJS.xhr({
+        return WinJS.xhr({
             type: "POST",
             url: "https://api.projectoxford.ai/face/v0/identifications",
             data: JSON.stringify(idReq),
@@ -151,15 +165,17 @@
                 "Content-Type": "application/json",
                 "Ocp-Apim-Subscription-Key": "d3151dd7f3314a848baa820db31fac11"
             }
-        }).done(
+        }).then(
         function completed(xhr) {
             var identifications = JSON.parse(xhr.responseText);
-            if (identifications && identifications.candidates) {
+            if (identifications && identifications.length && identifications[0].candidates && identifications[0].candidates.length) {
+                var candidate = identifications[0].candidates[0];
                 displayMessage("Person Found");
+                return { personId: candidate.personId, isNew: false };
 
                 isAnalyzing = false;
             } else {
-                createPerson(faceIds)
+                return createPersonAsync(faceIds);
             }
         },
         function error(request) {
@@ -169,10 +185,10 @@
         });
     }
 
-    function createPerson(faceIds) {        
+    function createPersonAsync(faceIds) {
         var person = { faceIds: faceIds, name: "UNKNOWN" };
 
-        WinJS.xhr({
+        return WinJS.xhr({
             type: "POST",
             url: "https://api.projectoxford.ai/face/v0/persongroups/1/persons",
             data: JSON.stringify(person),
@@ -180,12 +196,12 @@
                 "Content-Type": "application/json",
                 "Ocp-Apim-Subscription-Key": "d3151dd7f3314a848baa820db31fac11"
             }
-        }).done(
+        }).then(
         function completed(xhr) {
             var person = JSON.parse(xhr.responseText);
             if (person) {
                 displayMessage("Person Created");
-                trainModel();
+                return { personId : person.personId, isNew : true };
             } else {
 
                 isAnalyzing = false;
@@ -201,16 +217,15 @@
 
     function trainModel() {
 
-        WinJS.xhr({
+        return WinJS.xhr({
             type: "POST",
             url: "https://api.projectoxford.ai/face/v0/persongroups/1/training",
             headers: {
                 "Content-Type": "application/json",
                 "Ocp-Apim-Subscription-Key": "d3151dd7f3314a848baa820db31fac11"
             }
-        }).done(
+        }).then(
         function completed(xhr) {
-
             isAnalyzing = false;
             displayMessage("Model updated");
         },
